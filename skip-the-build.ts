1713ddd297d22b084c04625ce16756c46dd72b21
Steven Pautz <spautz@gmail.com>
2026-03-03 19:42:54 -0500

    Prepare release (automated) (#10)
    * Prepare release (automated)
    
    * Un-bump unchanged packages
    
    ---------
    
    Co-authored-by: spautz <243089+spautz@users.noreply.github.com>

--- skip-the-build.ts
+++ skip-the-build.ts
@@ -1,11 +1,11 @@
-import { presets, type SkipTheBuildConfig } from 'skip-the-build';
+import { defineSkipTheBuildConfig, presets, type SkipTheBuildConfigObject } from 'skip-the-build';
 
-const skipTheBuildConfig: SkipTheBuildConfig = {
+const skipTheBuildConfig: SkipTheBuildConfigObject = defineSkipTheBuildConfig({
   // For this local demo we only look at the SKIP_THE_BUILD env var
   extend: presets.envVarOnly,
   settings: {
     exportConditionName: 'local-dev',
   },
-};
+});
 
 export default skipTheBuildConfig;
